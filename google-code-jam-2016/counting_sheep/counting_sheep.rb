3fd5a396186fafe6731125162524a720db46dc04
Bob Carson <bob@carvinesoftware.com>
2016-04-09 10:53:12 -0400

    Adds counting sheep solution.

---
+++ google-code-jam-2016/counting_sheep/counting_sheep.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+def digits(n)
+  return Set.new([0]) if n == 0
+
+  digits = Set.new
+  while n > 0
+    digits.add(n % 10)
+    n = n / 10
+  end
+  digits
+end
+
+def count_sheep(n)
+  return "INSOMNIA" if n == 0
+
+  i = n
+  digits_seen = Set.new()
+  while true
+    digits_seen = digits_seen.union(digits(i))
+    return i if digits_seen.count == 10
+    i += n
+  end
+end
+
+def output(num, value)
+
+end
+
+lines = STDIN.read.split("\n")
+number_of_cases = lines.shift.to_i
+
+number_of_cases.times do |index|
+  n = lines.shift.to_i
+  value = count_sheep(n)
+  puts "CASE \##{index + 1}: #{value}"
+end
