7c9d8979d1a65fc80e8e7ac1cb13c46ec4c8b3a6
Bob Carson <bob@joinroot.com>
2020-05-02 04:34:57 -0400

    Code Jam 2020 Round 1B

--- google-code-jam-2020-round-1b/A/solve.rb
+++ google-code-jam-2020-round-1b/A/solve.rb
@@ -1,7 +1,147 @@
 #!/usr/bin/env ruby
 
 class Solver
-  def solve(array)
+  def solve(input)
+    factors = input.map { |i| _simplest_positivie(i) }
+    while factors[0].length < factors[1].length
+      factors[0] << 0
+    end
+    while factors[1].length < factors[0].length
+      factors[1] << 0
+    end
+    factors[0] << 0
+    factors[0] << 0
+    factors[1] << 0
+    factors[1] << 0
+
+    used = _used(factors)
+
+    return "IMPOSSIBLE" unless _solvable?(used)
+
+    while !_solved?(used)
+      first_fix = used.index { |i| i != 1 }
+      if used[first_fix] == 0
+        _flip_and_add_opposite!(factors, used, first_fix - 1)
+        used = _used(factors)
+      else
+        raise "Unexpected value #{used[first_fix]}" if used[first_fix] != 2
+        _flip_and_remove_opposite!(factors, used, first_fix - 1)
+        used = _used(factors)
+      end
+    end
+
+    # return factors
+
+    if input[0] < 0
+      factors[0] = factors[0].map { |i| i * -1 }
+    end
+
+    if input[1] < 0
+      factors[1] = factors[1].map { |i| i * -1 }
+    end
+
+
+    answer = []
+    runner = 0
+    while runner < factors[0].length
+      if factors[0][runner] == 1
+        answer << "E"
+      elsif factors[0][runner] == -1
+        answer << "W"
+      elsif factors[1][runner] == 1
+        answer << "N"
+      elsif factors[1][runner] == -1
+        answer << "S"
+      end
+      runner += 1
+    end
+
+    answer.join("")
+  end
+
+  def _flip_and_remove_opposite!(factors, used, index)
+    # puts "_flip_and_remove_opposite #{index}, #{factors}, #{used}"
+    if factors[0][index] != 0
+      factors[0][index] = -1 * factors[0][index]
+      next_index = index + 1
+      while factors[0][next_index] == 1
+        factors[0][next_index] = 0
+        next_index += 1
+      end
+      raise "Expected 0 #{factors[0][next_index]}" unless factors[0][next_index] == 0
+      factors[0][next_index] = -1 * factors[0][index]
+    else
+      raise "Expected non-zero #{factors[1][index]}" unless factors[1][index] != 0
+      factors[1][index] = -1 * factors[1][index]
+      next_index = index + 1
+      while factors[1][next_index] == 1
+        factors[1][next_index] = 0
+        next_index += 1
+      end
+      raise "Expected 0 got #{factors[1][next_index]}" unless factors[1][next_index] == 0
+      factors[1][next_index] = -1 * factors[1][index]
+    end
+  end
+
+  def _flip_and_add_opposite!(factors, used, index)
+    # puts "_flip_and_add_opposite #{index}, #{factors}, #{used}"
+    if factors[0][index] != 0
+      factors[0][index] = -1 * factors[0][index]
+      factors[0][index + 1] = -1 * factors[0][index]
+    else
+      raise "Expected non-zero #{factors[1][index]}" unless factors[1][index] != 0
+      factors[1][index] = -1 * factors[1][index]
+      factors[1][index + 1] = -1 * factors[1][index]
+    end
+  end
+
+  def _used(factors)
+    used = []
+    factors[0].each_with_index do |element, index|
+      used[index] = factors[0][index].abs + factors[1][index].abs
+    end
+    used
+  end
+
+  def _solved?(used)
+    runner = 0
+    seen_zero = false
+    while runner < used.length
+      if (used[runner] == 1 && seen_zero) || used[runner] == 2
+        return false
+      end
+      if used[runner] != 1
+        seen_zero = true
+      end
+      runner += 1
+    end
+
+    true
+  end
+
+  def _solvable?(used)
+    used[0] == 1
+  end
+
+  def _simplest_positivie(n)
+    return [] if n == 0
+
+    left = n.abs
+    exp = Math.log2(left).floor
+    answer = []
+    while exp >= 0
+      raise "Expected positive left #{left}" if left < 0
+      power = 2 ** exp
+        # puts "left #{left} exp #{exp} power #{power} answer #{answer}"
+      if power <= left
+        left -= power
+        answer << 1
+      else
+        answer << 0
+      end
+      exp -= 1
+    end
+    answer.reverse
   end
 end
 
@@ -9,7 +149,8 @@ lines = STDIN.read.split("\n")
 number_of_cases = lines.shift.to_i
 
 number_of_cases.times do |index|
-  input = lines.shift.to_i
+  input = lines.shift.split(" ").map(&:to_i)
   value = Solver.new.solve(input)
   puts "Case \##{index + 1}: #{value}"
 end
+# 100.times { puts "#{rand(-100..100)} #{rand(-100..100)}" }
--- google-code-jam-2020-round-1b/B/solve.rb
+++ google-code-jam-2020-round-1b/B/solve.rb
@@ -1,15 +1,79 @@
 #!/usr/bin/env ruby
 
 class Solver
-  def solve(array)
+  def solve(a)
+    if a == 999999995
+      _problem_set_1
+    else
+      _problem_set_2(a)
+    end
+
+  end
+
+  def _problem_set_2(a)
+    _find_x
+  end
+
+  def _find_x
+    increment = 25
+    value = 50
+    while increment > 1
+      _put_err("value #{value} increment #{increment}")
+      guess = "#{value} #{0}"
+      STDOUT.puts(guess)
+      STDOUT.flush
+
+      feedback = STDIN.gets
+      _put_err(feedback)
+      if feedback.start_with?("MISS")
+        value += increment
+      else
+        value -= increment
+      end
+
+      increment = (increment / 2.0).ceil
+    end
+
+    _put_err("value #{value} increment #{increment}")
+    guess = "#{value} #{0}"
+    STDOUT.puts(guess)
+    STDOUT.flush
+
+    feedback = STDIN.gets
+    _put_err(feedback)
+    if feedback.start_with?("MISS")
+      value += increment
+    else
+      value -= increment
+    end
+
+    value
+  end
+
+  def _problem_set_1
+    (-5..5).each do |x|
+      (-5..5).each do |y|
+        guess = "#{x} #{y}"
+        STDOUT.puts(guess)
+        STDOUT.flush
+
+        feedback = STDIN.gets
+
+        return if feedback.start_with?("CENTER")
+        raise "Wrong answer #{feedback}" unless feedback.start_with?("MISS") || feedback.start_with?("HIT")
+      end
+    end
+  end
+
+  def _put_err(string)
+    STDERR.puts(string)
   end
 end
 
-lines = STDIN.read.split("\n")
-number_of_cases = lines.shift.to_i
+inputs = STDIN.gets.split(" ").map(&:to_i)
+number_of_cases = inputs[0]
+a = inputs[1]
 
 number_of_cases.times do |index|
-  input = lines.shift.to_i
-  value = Solver.new.solve(input)
-  puts "Case \##{index + 1}: #{value}"
+  value = Solver.new.solve(a)
 end
